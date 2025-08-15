5aa10cd17f7a1e9ba5c811876a9acf4e197e9207
harshmandan <harshsinghmandan@gmail.com>
2025-08-15 18:35:53 +0530

    Setup for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,18 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/svelte-overflow-fade' : ''
+		}
 	}
 };
 
